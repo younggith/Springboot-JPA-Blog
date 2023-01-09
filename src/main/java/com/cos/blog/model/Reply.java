a5b41363012abc9746e15bf385fc46d1cdc5de09
younggith <60962799+younggith@users.noreply.github.com>
2023-01-09 23:27:53 +0900

    JPA모델세팅 완료

---
+++ src/main/java/com/cos/blog/model/Reply.java
@@ -0,0 +1,42 @@
+package com.cos.blog.model;
+
+import java.sql.Timestamp;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder	//  빌더 패턴!!
+@Entity
+public class Reply {
+	@Id	// Primary key
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(nullable = false, length = 200)
+	private String content;
+	
+	@ManyToOne
+	@JoinColumn(name = "boardId")
+	private Board board;
+	
+	@ManyToOne
+	@JoinColumn(name = "userId")
+	private User user;
+	
+	@CreationTimestamp
+	private Timestamp createDate;
+}
---
+++ src/main/java/com/cos/blog/test/Member.java
@@ -0,0 +1,26 @@
+package com.cos.blog.test;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+//DB에서 값을 가져오기때문에 값이 변결될 일을 경우 사용
+//@RequiredArgsConstructor final로 선언된 필드만 할당됨
+@Data
+@NoArgsConstructor
+public class Member {
+	private int id;
+	private String username;
+	private String password;
+	private String email;
+	
+	@Builder
+	public Member(int id, String username, String password, String email) {
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.email = email;
+	}
+}
